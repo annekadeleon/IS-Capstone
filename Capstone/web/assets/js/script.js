4e700f8913be4b4ac7b0a81aded6af2e8f204705
Anneka de Leon <annsophdl@gmail.com>
2021-07-01 01:50:40 +0800

    Delete Capstone directory
    push not working

--- Capstone/web/assets/js/script.js
+++
@@ -1,8 +0,0 @@
- function display(){
- 	var username = document.getElementById("username").value;
- 	alert (" Hello "+ username + "\n Successfully Logged In");
- } 
-function display2(){
- 	var username = document.getElementById("username").value;
- 	alert (" Hello "+ username + "\n Log In to your email to verify account.");
- }
