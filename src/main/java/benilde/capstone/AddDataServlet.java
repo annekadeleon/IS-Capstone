b2f4c49512f4a032647a11d3828f1afc149973df
Anneka de Leon <annsophdl@gmail.com>
2021-07-18 13:42:59 +0800

    AddDataServlet redirects to data.jsp

--- src/main/java/benilde/capstone/AddDataServlet.java
+++ src/main/java/benilde/capstone/AddDataServlet.java
@@ -10,29 +10,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/AddDataServlet")
 public class AddDataServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public AddDataServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("data.jsp");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//String netSalesURL = request.getParameter("netSalesURL");
+		response.sendRedirect("data.jsp");
 	}
 
 }
